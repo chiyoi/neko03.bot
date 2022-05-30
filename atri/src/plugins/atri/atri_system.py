b5c7fa5a990d4a04a29c4cc2e64cadf7bfd0077f
CHIYOI <chiyoi2140@outlook.com>
2022-05-30 21:06:53 +0800

    Mon May 30 21:06:53 CST 2022

--- atri/src/plugins/atri/atri_system.py
+++ atri/src/plugins/atri/atri_system.py
@@ -3,12 +3,12 @@ import os
 import nonebot
 import nonebot.adapters.onebot.v11 as onebot
 
-atri_system = nonebot.on_command("system", permission=nonebot.permission.SUPERUSER, priority=30)
+atri_system = nonebot.on_command("system", permission=nonebot.permission.SUPERUSER, block=True, priority=30)
 
 @atri_system.handle()
 async def atri_system_session(event: onebot.Event, matcher: nonebot.matcher.Matcher):
     expr = event.get_plaintext()[8:]
     output = os.popen(expr).read()
     if len(output) == 0:
-        output = "empty output"
+        output = "<nil>"
     await matcher.send(output)
