450f5b9f4d57aac4249d53065b3d9aaafbb1905b
CHIYOI <chiyoi2140@outlook.com>
2022-05-30 21:36:55 +0800

    Mon May 30 21:36:55 CST 2022

--- atri/bot.py
+++ atri/bot.py
@@ -1,4 +1,5 @@
 #!python3
+import os
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
 
@@ -8,3 +9,4 @@ driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 nonebot.load_plugin("src.plugins.atri")
 nonebot.load_plugin("src.plugins.eroira")
+os.mkdir("tmp")
