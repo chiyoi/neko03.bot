e564a62ee476aee3b435d69334d52dfab0540c9e
CHIYOI <chiyoi2140@outlook.com>
2022-06-20 20:19:54 +0800

    Mon Jun 20 20:19:54 CST 2022

--- atri/plugins/atri/atri_test.py
+++ atri/plugins/atri/atri_test.py
@@ -2,12 +2,9 @@ import nonebot
 import nonebot.adapters.onebot.v11 as onebot
 
 atri_test = nonebot.on_command("test", permission=nonebot.permission.SUPERUSER, block=False, priority=90)
-atri_test_notice = nonebot.on_notice(block=False, priority=1000)
 
 @atri_test.handle()
 async def atri_test_session1(event: onebot.Event, matcher: nonebot.matcher.Matcher):
     await matcher.send("event.dict: {}".format(event.dict()))
-
-@atri_test_notice.handle()
-async def atri_test_notice_session1(event: onebot.Event, matcher: nonebot.matcher.Matcher):
-    await matcher.send("event.dict: {}".format(event.dict()))
+    await matcher.send("event.get_event_name: {}".format(event.get_event_name()))
+    await matcher.send("event.get_event_description: {}".format(event.get_event_description()))
