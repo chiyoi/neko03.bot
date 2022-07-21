05e2d57778a5843d5be8ff6344004ff7fbba5c7e
chiyoi <chiyoi2140@outlook.com>
2022-07-21 16:03:24 +0800

    commit

--- build/update.sh
+++ build/update.sh
@@ -2,5 +2,5 @@
 git pull
 docker pull chiyoi/atri_bot-gocq
 cd /home/chiyoi/Projects/neko03.com/bot || return $?
-./build-image.sh
-./docker-entry.sh
+./build/build-image.sh
+./build/docker-entry.sh
